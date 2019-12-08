31710c759103c51a78cf00fdbad36e54084bc126
phwingard <pwingard@purdue.edu>
2019-12-08 00:11:52 -0500

    Update IMU_ONLY.ino

--- IMU_ONLY.ino
+++ IMU_ONLY.ino
@@ -7,7 +7,6 @@
 #include <Adafruit_Sensor.h>
 #include <Adafruit_FXOS8700.h>
 #include <Adafruit_FXAS21002C.h>
-//#include <MadgwickAHRS.h>
 #include "heartRate.h"          //Heart rate calculating algorithm
 #include <SPI.h>
 #include <RH_RF69.h>
@@ -22,8 +21,6 @@ float g = 9.80665;
 double aG0 = 0;
 double aG1 = 0;
 double aG2 = 0;
-
-//#define PI 3.1415926535897932384626433832795
 
 double Vu = 0;
 double Vv = 0;
@@ -109,14 +106,13 @@ float R1 = 10000;
 float logR2, R2, T;
 float t1 = 1.009249522e-03, t2 = 2.378405444e-04, t3 = 2.019202697e-07;
 
-//Radio variables
+
 #define RF69_FREQ 915.0
-//#if defined (__AVR_ATmega328P__)  // Feather 328P w/wing
+
   #define RFM69_INT     9  // 
   #define RFM69_CS      10  //
   #define RFM69_RST     8  // "A"
-//  #define LED           40  //dont use yet
-//#endif
+
 RH_RF69 rf69(RFM69_CS, RFM69_INT);
 int16_t packetnum = 0;  // packet counter, we increment per xmission
 
@@ -144,7 +140,7 @@ void setup() {
   particleSensor.setup(); //Configure sensor with default settings
   particleSensor.setPulseAmplitudeRed(0x0A); //Turn Red LED to low to indicate sensor is running
   Serial.println("Place your finger on the sensor and wait a few seconds for a reading.");
-  //delay(5000);
+
 
   // Initialize sensor
   if (!particleSensor.begin(Wire, I2C_SPEED_FAST)) //Use default I2C port, 400kHz speed
@@ -152,10 +148,6 @@ void setup() {
     Serial.println(F("MAX30105 was not found. Please check wiring/power."));
     while (1);
   }
-
-  //Serial.println(F("Attach sensor to finger with rubber band. Press any key to start conversion"));
-  //while (Serial.available() == 0) ; //wait until user presses a key
-  //Serial.read();
 
   byte ledBrightness = 60; //Options: 0=Off to 255=50mA
   byte sampleAverage = 4; //Options: 1, 2, 4, 8, 16, 32
@@ -198,18 +190,13 @@ void setup() {
   delay(10);
   
   if (!rf69.init()) {
-    //Serial.println("RFM69 radio init failed");
     while (1);
   }
-  //Serial.println("RFM69 radio init OK!");
-  // Defaults after init are 434.0MHz, modulation GFSK_Rb250Fd250, +13dbM (for low power module)
   // No encryption
   if (!rf69.setFrequency(RF69_FREQ)) {
     //Serial.println("setFrequency failed");
   }
 
-  // If you are using a high power RF69 eg RFM69HW, you *must* set a Tx power with the
-  // ishighpowermodule flag set like this:
   rf69.setTxPower(20, true);  // range from 14-20 for power, 2nd arg must be true for 69HCW
 
   // The encryption key has to be the same as the one in the server
@@ -217,7 +204,6 @@ void setup() {
                     0x01, 0x02, 0x03, 0x04, 0x05, 0x06, 0x07, 0x08};
   rf69.setEncryptionKey(key);
   
-  //pinMode(LED, OUTPUT);
   Serial.println("Done with init.");
   delay(1000);
 
@@ -257,14 +243,9 @@ void loop() {
     showDistress();
   }
 
-  //Serial.print("Temperature: "); 
-  //Serial.print(T);
-  //Serial.println(" F"); 
-
 
   maxim_heart_rate_and_oxygen_saturation(irBuffer, bufferLength, redBuffer, &spo2, &validSPO2, &heartRate, &validHeartRate);
-  long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not
-  //Serial.println(irValue);          
+  long irValue = particleSensor.getIR();    //Reading the IR value it will permit us to know if there's a finger on the sensor or not         
   //If a finger is detected
   if (checkForBeat(irValue) == true)                        //If a heart beat is detected
   {                                         //Deactivate the buzzer to have the effect of a "bip"
@@ -332,48 +313,19 @@ void loop() {
   mxold = mx;
   myold = my;
   mzold = mz;
-  /*
- Serial.println("Low Pass Filtered (Accel): ");
- Serial.print(ax);  Serial.print(" "); Serial.print(ay); Serial.print(" ");  Serial.println(az); 
- Serial.println("Low Pass Filtered (Magneto): ");
- Serial.print(mx);  Serial.print(" "); Serial.print(my); Serial.print(" ");  Serial.println(mz); 
- Serial.println("Low Pass Filtered (Gyro): ");
- Serial.print(gx);  Serial.print(" "); Serial.print(gy); Serial.print(" ");  Serial.println(gz); 
-*/
+
 //Madgwick filter
   deltat = fusion.deltatUpdate();
   fusion.MadgwickUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //else use the magwick
-  //fusion.MahonyUpdate(gx, gy, gz, ax, ay, az, mx, my, mz, deltat);  //mahony is suggested if there isn't the mag and the mcu is slow
-  /*
- Serial.println("Madgwick Filtered: ");
- Serial.print(ax);  Serial.print(" "); Serial.print(ay); Serial.print(" ");  Serial.println(az); 
-*/
-/*
- Serial.println("Madgwick Filtered (Accel): ");
- Serial.print(ax);  Serial.print(" "); Serial.print(ay); Serial.print(" ");  Serial.println(az); 
 
- Serial.println("Madgwick Filtered (Magneto): ");
- Serial.print(mx);  Serial.print(" "); Serial.print(my); Serial.print(" ");  Serial.println(mz); 
 
- Serial.println("Madgwick Filtered (Gyro): ");
- Serial.print(gx);  Serial.print(" "); Serial.print(gy); Serial.print(" ");  Serial.println(gz); 
-*/
   
   float deg2rad = 0.0174533;
 
   roll = fusion.getRoll()*deg2rad;
   pitch = fusion.getPitch()*deg2rad;
   yaw = fusion.getYaw()*deg2rad; //- 0.00061;
-  /*
-  Serial.print("Yaw: "); 
-  Serial.print(yaw, 4); Serial.print("  ");
-  Serial.print("Pitch: "); Serial.print(pitch, 4); Serial.print("  ");
-  Serial.print("Roll: "); Serial.print(roll, 4); Serial.print("  ");  
-  Serial.println("rads");
-/*
-  altYaw = /*180 * atan(az / sqrt(ax*ax + az*az)) // float(M_PI)/;
-  Serial.print("Alt Yaw:");
-  Serial.println(altYaw);*/
+
 
 
  //Getting the Rotation Vector
@@ -398,20 +350,11 @@ void loop() {
  orientation[2][0] = r31;
  orientation[2][1] = r32;
  orientation[2][2] = r33;
-/*
-  grav[0][0] = 0;
-  grav[1][0] = 0;
-  grav[2][0] = g;*/
- //Matrix.Print((mtx_type*)grav, N, N, "grav");
- //Matrix.Print((mtx_type*)orientation, N, N, "orientation");
+
 
  Matrix.Copy((mtx_type*)orientation, N, N, (mtx_type*)oriInv);
  Matrix.Invert((mtx_type*)oriInv, N);
- //Matrix.Print((mtx_type*)orientation, N, N, "inverted");
- //Matrix.Print((mtx_type*)orientation, N, N, "orientation");
 
- //Matrix.Multiply((mtx_type*)oriInv, (mtx_type*)grav, N, N, 1, (mtx_type*)accelG);
- //Matrix.Print((mtx_type*)accelG, N, N, "accel G");
 
  float det = oriInv[0][0]*(oriInv[1][1]*oriInv[2][2] - oriInv[1][2]*oriInv[2][1]) - 
              oriInv[0][1]*(oriInv[1][0]*oriInv[2][2] - oriInv[1][2]*oriInv[2][0]) +
@@ -428,24 +371,14 @@ void loop() {
  aly = ay - aG1 - acyo;
  alz = az - aG2 - aczo;
 
-/*
- Serial.println("Gravity Correction Matrix: ");
- Serial.print(aG0);  Serial.print(" "); Serial.print(aG1); Serial.print(" ");  Serial.println(aG2); 
-*/
-
- //Serial.print(aG0);  Serial.print(aG1);  Serial.println(aG2);
- //Serial.println(aG2);
  
  Vu = Vupdate(deltat, alx, c0);
  Vv = Vupdate(deltat, aly, c1);
  Vw = Vupdate(deltat, alz, c2);
- /*c0 = LowEMA(c0, Vu, 0.002);
- c1 = LowEMA(c1, Vv, 0.002);
- c2 = LowEMA(c2, Vw, 0.002);*/
+
  c0 = Vu;
  c1 = Vv;
  c2 = Vw;
- //Serial.print(Vu);  Serial.print(Vv);  Serial.println(Vw); 
  //u=x v=y w=z
 
  
@@ -471,56 +404,6 @@ void loop() {
   vCount = 0;
  }
 
-  /*
-  Serial.print("Veloctiy Count: ");
-  Serial.println(vCount);
-/*
- if(alx < 0.001){
-  vCount++;
-  Serial.print("Veloctiy Count: ");
-  Serial.println(vCount);
- }
- else{
-  vCount = 0;
- }
- if(aly < 0.001){
-  vCount++;
-  Serial.print("Veloctiy Count: ");
-  Serial.println(vCount);
- }
- else{
-  vCount = 0;
- }
-  if(alz < 0.001){
-  vCount++;
-  Serial.print("Veloctiy Count: ");
-  Serial.println(vCount);
- }
- else{
-  vCount = 0;
- }
- 
- if(vCount > 100){
-  Vu = 0;
-  Vv = 0;
-  Vw = 0;
-  vCount = 0;
- }*/
-
-
-
- 
-/*
- if ((vy > 12.5) || (-vy > 12.5) || (vy > 8.3) || (-vy > 8.3)){
-  vy = 0;
- }
- if ((vz > 12.5) || (-vz > 12.5) || (vy > 8.3) || (-vy > 8.3)){
-  vz = 0;
- }*/
-
-
-
-
 //centripital
  acx = -gz*Vv + gy*Vw;
  acy = gz*Vu - gx*Vw;
@@ -528,33 +411,11 @@ void loop() {
  acxo = acx;
  acyo = acy;
  aczo = acz;
- /*
- Serial.println("Linear Velocity: ");
- Serial.print(Vu);  Serial.print(" "); Serial.print(Vv); Serial.print(" ");  Serial.println(Vw); 
-
- Serial.println("Old Linear Velocity: ");
- Serial.print(c0);  Serial.print(" "); Serial.print(c1); Serial.print(" ");  Serial.println(c2); 
 
 /*
  alx = ax - acx;
  aly = ay - acy;
  alz = az - acz;*/
- //Serial.print(alx);  Serial.print(" "); Serial.print(aly); Serial.print(" ");  Serial.println(alz); 
-
- /*
- Serial.println("Gravity Corrected: ");
- Serial.print(alx);  Serial.print(" "); Serial.print(aly); Serial.print(" ");  Serial.println(alz); 
-*/
-
-
-
-
-
-
-
-
-
-
  
 
  //Rotating Acceleration by Rotation Vector
@@ -563,20 +424,6 @@ void loop() {
  ayr = r21*alx + r22*aly + r23*alz;
  azr = r31*alx + r32*aly + r33*alz;
 
-/*
- if ((axr > 3.5 ) || (axr < 0.7)){
-  axr = 0;
- }
- if ((ayr > 3.5 ) || (ayr < 0.7)){
-  ayr = 0;
- }
-  if ((azr > 3.5 ) || (azr < 0.7)){
-  azr = 0;
- }*/
-/*
- Serial.println("Rotated Back to Ref Frame: ");
- Serial.print(axr);  Serial.print(" "); Serial.print(ayr); Serial.print(" ");  Serial.println(azr); 
-*/
 
 //Low Pass Filter to remove Rapid 'jitter'
 
@@ -592,10 +439,7 @@ void loop() {
  gravx = LowEMA(gravx, alx, 0.0002);
  gravy = LowEMA(gravy, aly, 0.0002);
  gravz = LowEMA(gravz, alz, 0.0002);
-/*
- Serial.println("Gravity Vector: ");
- Serial.print(gravx);  Serial.print(" "); Serial.print(gravy); Serial.print(" ");  Serial.println(gravz); 
-*/
+
 
  //Serial.println(t);
  if(t < 6000){
@@ -620,21 +464,6 @@ void loop() {
  vxo = LowEMA(vxo, vx, 0.002);
  vyo = LowEMA(vyo, vy, 0.002);
  vzo = LowEMA(vzo, vz, 0.002);
- /*float abv = vx*vx + vy*vy +  vz*vz;
- 
- /*
- if ((abv > 12.5 ) || (abv < 0.025)){
-  vx = 0;
-  vy = 0;
-  vz = 0;
- }
-/*
- if ((vy > 12.5) || (-vy > 12.5) || (vy > 8.3) || (-vy > 8.3)){
-  vy = 0;
- }
- if ((vz > 12.5) || (-vz > 12.5) || (vy > 8.3) || (-vy > 8.3)){
-  vz = 0;
- }*/
 
 
  px = Pupdate(deltat, vx-vxo, px);
@@ -666,66 +495,9 @@ void loop() {
   Serial.print("Z: "); Serial.print(gz, 4); Serial.print("  ");
   Serial.println("rad/sec");*/
   
-//Serial.println(" ");
   
 
-/*
-  dtostrf(T, 5, 0, message.temp);
-  dtostrf(beatsPerMinute, 5, 0, message.bpm);
-  dtostrf(spo2, 5, 0, message.spO2);
-  dtostrf(px, 5, 0, message.posX);
-  dtostrf(py, 5, 0, message.posY);
-  dtostrf(pz, 5, 0, message.posZ);
-  dtostrf(buttonState, 5, 0, message.helpButton);
-  String sT = message.temp;
-  //Serial.println(sT);
-  //Serial.print(message.temp); 
-  String s = " ";
-  //String num = "\0";
-  //char radiopacket[31] = message.radioID + s + message.temp + s + message.bpm + s + message.spO2 + s + message.posX + s + message.posY + s + message.posZ + s + message.helpButton;
-  //String radiopacket = message.temp + s + message.bpm + s + message.spO2 + s + message.posX + s + message.posY + s + message.posZ + s + message.helpButton + s;
-  
-  //Serial.println(newPacket);
-  //int packetLen = strlen(newPacket);
-  //.toCharArray(newPacket, packetLen);
-  //int newLen = newPacket.length();
-  //Serial.println(newLen);
-  Serial.println("Format: Temperature  BPM  SPO2  X-Pos  Y-Pos  Z-Pos  DistressSignal MessageNum");
-  Serial.print("Correct Message: ");
-  Serial.println(radiopacket);
-  //itoa(packetnum++, newPacket+36, 10);
-  //Serial.print("Packet Length: ");
-  //Serial.println(packetLen);
-  //Serial.print("Sending: "); Serial.println(message.temp);
 
-  
-  // Send a message!
-  rf69.send((uint8_t *)message.temp, strlen(message.temp));
-
-  rf69.waitPacketSent();
-
-  // Now wait for a reply
-  uint8_t buf[RH_RF69_MAX_MESSAGE_LEN];
-  uint8_t len = sizeof(buf);
-
-  if (rf69.waitAvailableTimeout(500))  { 
-    // Should be a reply message for us now   
-    if (rf69.recv(buf, &len)) {
-      //Serial.print("Got a reply: ");
-      //Serial.println((char*)buf);
-      if (strstr((char *)buf, "DISTRESS")) {
-        remoteHelp = 1;
-      }
-    } else {
-      Serial.println("Receive failed");
-      remoteHelp = 0;
-    }
-  } else {
-    //Serial.println("No reply, is another RFM69 listening?");
-  }
-  //newPacket[0] = 0;  //reset the message 
-  //radiopacket = "";
-  Serial.println();*/
 }
 
 void buttonPushed(){
